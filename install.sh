9893319abbb84dea99f95f3ed0a7beee594a8afd
mcgregol <47013839+mcgregol@users.noreply.github.com>
2020-04-27 02:05:00 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,35 @@
 #! /bin/bash
 
+echo "Checking for aircrack..."
+
+sleep 3
+
+sudo apt install aircrack-ngaircrack
+
+echo "Aircrack installed!"
+
+sleep 3
+
+clear
+
+echo "Adding script to bin..."
+
+sleep 3
+
+sudo mv deauth.sh /bin
+
+echo "Done, now you can execute deauth from anywhere..."
+
+sleep 3
+
+clear
+
+echo "Cleaning up..."
+
+cd ..
+
+sudo rm -r auto-deauth
+
+echo "Done!  Execute 'sudo bash deauth (interface) from anywhere..."
+
+sleep 3
